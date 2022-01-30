9360756479a8a5c1338472aad64231d4915a8bf9
Dima Rogov <ds.rogv@gmail.com>
2022-01-30 22:43:04 +0300

    add lombok to generate User sources

---
+++ src/main/lombok/User.java
@@ -0,0 +1,17 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
